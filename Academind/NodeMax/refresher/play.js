0fdff1744015657ab596bf3bcc255d3fc5059dd8
Winterstorm <thunpwnz@gmail.com>
2019-01-19 23:24:09 +0400

    Playing with scope

--- Academind/NodeMax/refresher/play.js
+++ Academind/NodeMax/refresher/play.js
@@ -1,23 +1,29 @@
-const fetchData = () => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Done!');
-    }, 1500);
-  });
-  return promise;
-};
+// const fetchData = () => {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('Done!');
+//     }, 1500);
+//   });
+//   return promise;
+// };
 
-setTimeout(() => {
-  console.log('Timer is done!');
-  fetchData()
-    .then(text => {
-      console.log(text);
-      return fetchData();
-    })
-    .then(text2 => {
-      console.log(text2);
-    });
-}, 2000);
+// setTimeout(() => {
+//   console.log('Timer is done!');
+//   fetchData()
+//     .then(text => {
+//       console.log(text);
+//       return fetchData();
+//     })
+//     .then(text2 => {
+//       console.log(text2);
+//     });
+// }, 2000);
 
-console.log('Hello!');
-console.log('Hi!');
+// console.log('Hello!');
+// console.log('Hi!');
+let command = 'hi'
+console.log(command)
+{
+  let command = 'Hi from inside'
+  console.log(command)
+}
